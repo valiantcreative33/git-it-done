769b6fc48d7987205511f5b4e1896c193d3ef07c
Ruben Matamoros <ruben@valiantcreative.net>
2020-11-24 00:19:36 -0500

    created list of featured repos using javascript, html, and css

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
@@ -88,5 +89,30 @@ var getUserRepos = function(user) {
     console.log(repos);
     console.log(searchTerm);
   };
+
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language); 
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    });
+  };
+
+  var buttonClickHandler = function() {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+      getFeaturedRepos(language);
+    
+      // clear old content
+      repoContainerEl.textContent = "";
+    }
+  }
   
   userFormEl.addEventListener("submit", formSubmitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
